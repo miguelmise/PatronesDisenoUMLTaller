64353b920ad1078159247be382f89de4ec4cea79
sebyaguana <85586939+sebyaguana@users.noreply.github.com>
2021-12-09 16:35:27 -0500

    Add files via upload
    Codigo Chain Responsability

---
+++ Reclamos_Manejador.java
@@ -0,0 +1,10 @@
+package Reclamos_Paquete;
+
+public interface Reclamos_Manejador extends Tecnico_Departamento {
+
+	public void loadRequest();
+
+	public void getNext();
+
+	public void setNext();
+}
