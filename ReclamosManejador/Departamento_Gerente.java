5c41008fd6fbf928b9a109e12cb2cddebfd21cd0
sebyaguana <85586939+sebyaguana@users.noreply.github.com>
2021-12-09 16:39:14 -0500

    Add files via upload

---
+++ ReclamosManejador/Departamento_Gerente.java
@@ -0,0 +1,17 @@
+package Reclamos_Paquete;
+
+public class Departamento_Gerente {
+	private Object _reclamosManejador_rc1;
+
+	public void loadRequest() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void getNext() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setNext(Object aReclamo) {
+		throw new UnsupportedOperationException();
+	}
+}
